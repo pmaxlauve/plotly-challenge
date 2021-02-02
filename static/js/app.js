2444df094448e5ac6fafa868e77e61bc24e44943
pmaxlauve <71564864+pmaxlauve@users.noreply.github.com>
2021-02-02 14:56:58 -0600

    complete-- just needs commets

--- static/js/app.js
+++ static/js/app.js
@@ -3,30 +3,7 @@ var dataLoc = "./data/samples.json";
 
 
 
-// var trace = {
-//     x: [2, 4],
-//     y: ["a", "b"],
-//     name: "Top microbes by CFU's",
-//     type: "bar",
-//     orientation: "h"
-//   };
 
-// console.log(trace)
-
-//   var data = [trace];
-
-
-// // Apply the group bar mode to the layout
-// var layout = {
-//     title: "Top microbes by CFU's",
-//     margin: {
-//     l: 100,
-//     r: 100,
-//     t: 100,
-//     b: 100
-//     }
-
-// Plotly.newPlot("bar", data, layout);
 
 
 
@@ -60,6 +37,8 @@ d3.json(dataLoc).then(function(d) {
     
 
         var sData= d.samples;
+        var metaData = d.metadata;
+    
 
 
 function optionChanged() {
@@ -67,34 +46,104 @@ function optionChanged() {
     // Assign the value of the dropdown menu option to a variable
     var dataset = dropdownMenu.property("value");
     // Initialize an empty array for the country's data
-    var visData = {lab:[], val:[]};
+    var visData = {lab:[], val:[], id:[]};
   
     
     sData.forEach(function(d) {
         if (d.id == dataset) {
             visData.lab = d.otu_ids.map(x => `OTU ${x}`);
 
-            visData.val = d.sample_values.slice(0,10);
+            visData.id = d.otu_ids;
+
+            visData.val = d.sample_values;
         }
-    })
+    });
+
+
+    metaData.forEach(function(d) {
+      if (d.id == dataset) {
+        demData = d
+      }
+    });
+
+    
+
+    
+
+    
+
 
     
 
     updateBar(visData);
+    updateBubble(visData);
+    updateDemographics(demData);
     
   }
 
+function updateDemographics(d) {
+  var demArr = [
+    `ID: ${d.id}`,
+    `Ethnicity: ${d.ethnicity}`,
+    `Gender: ${d.gender}`,
+    `Age: ${d.age}`,
+    `City/State: ${d.location}`,
+    `Type: ${d.bbtype}`,
+    `Wash Frequency: ${d.wfreq}`
+  ];
+
+  d3.select("#sample-metadata")
+    .append("ul")
+    .selectAll("li")
+    .data(demArr)
+    .enter()
+    .append("li")
+    .text(function(d) {
+      return d;
+    });
+
+  console.log(demArr);
+}
+
+function updateBubble(d) {
+
+
+  var trace1 = {
+    x: d.id,
+    y: d.val,
+    mode: 'markers',
+    marker: {
+      size: d.val,
+      color: d.id
+    },
+    text: d.lab
+  };
+
+  var data = [trace1];
+
+  var layout = {
+    title: "Complete Belly-Button Flora",
+    height: 500,
+    width: 1100
+  };
+
+  Plotly.newPlot("bubble", data, layout)
+
+}
+
 function updateBar(d) {
-    console.log(d.lab)
-    console.log(d.val)
+
+
+
 
 
 
     var trace = {
-        x: d.val,
-        y: d.lab,
+        x: d.val.slice(0, 10).reverse(),
+        y: d.lab.slice(0, 10).reverse(),
         type: "bar",
-        orientation: "h"
+        orientation: "h",
+        text: d.lab.slice(0, 10).reverse()
       };
 
     console.log(trace)
@@ -105,15 +154,9 @@ function updateBar(d) {
     // Apply the group bar mode to the layout
     var layout = {
         title: "Top microbes by CFU's",
-        margin: {
-        l: 100,
-        r: 100,
-        t: 100,
-        b: 100
-        }
 
-    Plotly.newPlot("bar", data, layout);
   };
+  Plotly.newPlot("bar", data, layout);
   
 };
 
