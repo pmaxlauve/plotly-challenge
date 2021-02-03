a743462dcb40da723de3974b5df1f5e51ad342e4
pmaxlauve <71564864+pmaxlauve@users.noreply.github.com>
2021-02-02 19:14:06 -0600

    finished

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,12 @@
+
+//set up variable for samples.json location
 var dataLoc = "./data/samples.json";
 
 
-
+//use d3.json to bring in the data
 d3.json(dataLoc).then(function(d) {
 
+  //variable for blank demographics box
   var demBlank = [
     `ID: `,
     `Ethnicity: `,
@@ -15,7 +18,7 @@ d3.json(dataLoc).then(function(d) {
   ];
 
 
-  
+    //create the html for the demographics box
     d3.select('#sample-metadata')
     .append("ul")
     .selectAll("li")
@@ -26,9 +29,10 @@ d3.json(dataLoc).then(function(d) {
       return d;
     });
 
-
+    //varialbe for the subject ID's
     var idArr = d.names;
     
+    //create the html for options on the dropdown menu, and assign the ID's
     d3.select("#selDataset")
     .selectAll("option")
     .data(idArr)
@@ -42,51 +46,61 @@ d3.json(dataLoc).then(function(d) {
     });
 
     
+    //set up event for dropdown menu
     d3.selectAll("#selDataset").on("change", optionChanged);
 
     
-
+      // create variables for samples and metadata
         var sData= d.samples;
         var metaData = d.metadata;
     
 
-
+//create function optionChanged()
 function optionChanged() {
+    // locate the dropdown menu
     var dropdownMenu = d3.select("#selDataset");
     // Assign the value of the dropdown menu option to a variable
     var dataset = dropdownMenu.property("value");
-    // Initialize an empty array for the country's data
+    // create a dictionary to put sample data into
     var visData = {lab:[], val:[], id:[]};
   
-    
+    // get data for selected subject ID
     sData.forEach(function(d) {
+        //set up conditional to get only data for the given subject ID
         if (d.id == dataset) {
+
+            //get the label
             visData.lab = d.otu_ids.map(x => `OTU ${x}`);
-
+            //get the otu ID as an integer
             visData.id = d.otu_ids;
-
+            //get the value
             visData.val = d.sample_values;
         }
     });
 
-
+    //get the meta-data for each subject ID
     metaData.forEach(function(d) {
+      //set up conditional to get only data for the given subject ID
       if (d.id == dataset) {
+        //set the metadata dictionary equal to demData
         demData = d
       }
     });
 
-
+    //run functions to build the charts
     updateBar(visData);
     updateBubble(visData);
     updateDemographics(demData);
     updateGauge(demData)
     
   }
-
+// function to build the gauge
 function updateGauge(d) {
+
+  // assign variable for wash frequency
   var washes = d.wfreq;
 
+  // customize gauge
   var data = [
     {
       value: washes,
@@ -96,7 +110,7 @@ function updateGauge(d) {
       gauge: {
         bar: {
           color: "#336699",
-          thickness: .8
+          thickness: .75
         },
         axis: {range: [0, 9]},
         steps: [
@@ -114,15 +128,19 @@ function updateGauge(d) {
 
     }
   ];
-  
+  //set up the layout
   var layout = { width: 500, height: 300, margin: { t: 0, b: 0 } };
+
+  //create the plot
   Plotly.newPlot('gauge', data, layout);
 }
 
 
 
-
+// function to build the demographics box
 function updateDemographics(d) {
+
+  //create variable containing the list entries for the demographics box
   var demArr = [
     `ID: ${d.id}`,
     `Ethnicity: ${d.ethnicity}`,
@@ -133,10 +151,11 @@ function updateDemographics(d) {
     `Wash Frequency: ${d.wfreq}`
   ];
 
+
   var demInfo = d3.select('#sample-metadata');
 
-  
-    demInfo
+  //replace the initial data with the new data
+  demInfo
     .selectAll("li")
     .data(demArr)
     .text(function(d) {
@@ -146,10 +165,10 @@ function updateDemographics(d) {
 
 
 }
-
+//function to build the bubble chart
 function updateBubble(d) {
 
-
+  //customize the bubble plot
   var trace1 = {
     x: d.id,
     y: d.val,
@@ -161,21 +180,35 @@ function updateBubble(d) {
     text: d.lab
   };
 
+
   var data = [trace1];
 
+  //set up the layout
   var layout = {
     title: "Complete Belly-Button Flora",
     height: 500,
-    width: "100%"
+    width: "100%",
+    yaxis: {
+      title: {
+        text: "Colony Forming Units"
+      }
+    },
+    xaxis: {
+      title: {
+        text: "OTU ID"
+      }
+    }
   };
 
+  //plot the bubble plot
   Plotly.newPlot("bubble", data, layout)
 
 };
 
+//function to build the bar chart
 function updateBar(d) {
 
-
+    //customize the plot
     var trace = {
         x: d.val.slice(0, 10).reverse(),
         y: d.lab.slice(0, 10).reverse(),
@@ -184,14 +217,13 @@ function updateBar(d) {
         text: d.lab.slice(0, 10).reverse()
       };
 
-    console.log(trace)
-
-      var data = [trace];
-
+    var data = [trace];
+    //set up the layout
     var layout = {
         title: "Top microbes by CFU's",
-
-  };
+    
+      };
+  //plot the chart
   Plotly.newPlot("bar", data, layout);
   
 };
